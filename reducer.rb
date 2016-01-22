8528cce91b645bc03c6b01f2671cdb0d27cb2994
Nyi <serge@nyi.ch>
2016-01-22 12:20:48 +0100

    Fixed parsing for new feed

--- reducer.rb
+++ reducer.rb
@@ -2,8 +2,6 @@ require 'open-uri'
 require 'rss'
 require 'nokogiri'
 
-feed_file = File.read('feed_file_location.conf').strip!
-
 class Comic
   attr_accessor :title, :url, :date
 
@@ -23,17 +21,17 @@ class ComicsFeed
   def update_data
     feed_content = open('http://feeds.feedburner.com/DilbertDailyStrip')
 
-    feed = RSS::Parser.parse(feed_content)
-    @title = feed.channel.title
+    feed = RSS::Parser.parse(feed_content, false, false)
+    @title = feed.title
 
     feed.items.each do |item|
       comic = Comic.new
       comic.title = item.title
-      comic.date = item.date
+      comic.date = item.updated
 
-      website = Nokogiri::HTML(open(item.link))
+      website = Nokogiri::HTML(open(item.link.href))
 
-      website.xpath('//img[contains(@title, "The Dilbert Strip for")]/@src').each do |imgsrc|
+      website.xpath('//img[contains(@alt, "- Dilbert by ")]/@src').each do |imgsrc|
         comic.url = imgsrc
       end
 
@@ -51,9 +49,9 @@ class ComicsFeed
       @comics.each do |comic|
         maker.items.new_item do |item|
           item.link = ''
-          item.title = comic.title
-          item.updated = comic.date
-          item.summary = "<img src='http://dilbert.com/#{comic.url}' />"
+          item.title = comic.title.content
+          item.updated = comic.date.to_s
+          item.summary = "<img src='#{comic.url}' />"
         end
       end
     end
@@ -62,10 +60,6 @@ class ComicsFeed
   end
 end
 
-#todo:
-
-feed = ComicsFeed.new
-
 def get_days_not_updated(feed_file)
   if feed_file.nil? || feed_file.empty? || (not File.exist?(feed_file))
     100
@@ -73,6 +67,10 @@ def get_days_not_updated(feed_file)
     DateTime.now - File.mtime(feed_file).to_date
   end
 end
+
+
+feed_file = File.read('feed_file_location.conf').strip
+feed = ComicsFeed.new
 
 days_not_updated = get_days_not_updated(feed_file)
 if days_not_updated >= 1
